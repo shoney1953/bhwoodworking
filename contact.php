7f94d4714e24207835e351458c16bbc4fc101fb3
sheila honey <sheila_honey_5@hotmail.com>
2024-02-28 12:23:38 -0700

    initial commit

---
+++ contact.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" 
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Brian Hand's Custom Woodworking - Contact</title>
+    <!-- <link rel="icon" type="image/x-icon" href="favicon.ico"> -->
+</head>
+<body>
+<div class="container-section">
+<nav class="nav">
+    <div class="container">
+        <ul>
+        <li><a href="index.php">Home</a></li>
+      
+        </ul>
+    </div>
+</nav>
+
+        <div class="container">
+
+            <h1>Contact Brian</h1>
+            <h2>Phone: 512 751-7280</h2>
+
+            <br><a href="mailto:brianhand1@outlook.com?subject=Woodworking Information">
+                       Click to Email Brian</a>
+</div>
+
+</div>
+<?php
+  require 'footer.php';
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" 
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Brian Hand's Custom Woodworking</title>
+    <!-- <link rel="icon" type="image/x-icon" href="favicon.ico"> -->
+</head>
+<body>
+<div class="container-section">
+<nav class="nav">
+    <div class="container">
+        <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+
+        <div class="container">
+
+            <h1>Brian Hand Custom Woodworking</h1>
+
+            <h3>Located in SaddleBrooke Arizona,<br> Brian creates custom pieces from local wood sources.</h3><br>
+
+            <a href="https://sheilahoney.smugmug.com/Brian-Hands-Creations">Click to see Samples of his work</a><br><br>
+            <a href="mailto:brianhand1@outlook.com?subject=Woodworking Information">
+                       Click to Email Brian</a>
+       
+</div>
+
+</div>
+<?php
+  require 'footer.php';
+?>
+</body>
+</html>
